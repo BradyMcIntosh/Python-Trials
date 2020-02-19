190179c1d1d407bc048c2e3d45c7dbc834e04d24
Brady John McIntosh <36173519+BradyMcIntosh@users.noreply.github.com>
2020-02-19 16:47:20 -0500

    Various Updates

--- text_gen_rnn.py
+++ text_gen_rnn.py
@@ -8,4 +8,10 @@ import numpy as np
 import os
 import time
 
-tf.keras.utils.get_file()
+# Read, then decode for py2 compat.
+text = open('data/Irene Iddesleigh clean.txt', 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print ('Length of text: {} characters'.format(len(text)))
+
+# Take a look at the first 250 characters in text
+print(text[:250])
