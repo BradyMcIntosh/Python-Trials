bdf8e2f975d78679ced9e38a9c6c1ebd845ce9b7
Brady John McIntosh <36173519+BradyMcIntosh@users.noreply.github.com>
2020-02-20 01:06:53 -0500

    Added text parsing & value mapping

--- text_gen_rnn.py
+++ text_gen_rnn.py
@@ -15,3 +15,22 @@ print ('Length of text: {} characters'.format(len(text)))
 
 # Take a look at the first 250 characters in text
 print(text[:250])
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+# Show mapping value for each character
+print('{')
+for char,_ in zip(char2idx, range(len(vocab))):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+# Show how the first 13 characters from the text are mapped to integers
+print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:20]), text_as_int[:20]))
