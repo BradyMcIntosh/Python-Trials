856b7d504b6965d51329ef42df9e39e5dfe33e29
Brady John McIntosh <36173519+BradyMcIntosh@users.noreply.github.com>
2020-02-26 04:01:02 -0500

    Added debug mode, Improved html & regex editing

--- clean_text.py
+++ clean_text.py
@@ -3,25 +3,25 @@
 # Otherwise, this program will remove page numbers, images, redundant line breaks, and other markup elements.
 
 # TODO: Read all files in a "text-source" folder, clean and add to a "text-clean" folder
-# TODO: Remove chapter headers.
 # TODO: (in a new file) Combine various clean texts into larger files organized by author, genre, etc.
 
-import csv, re
+import re, html, unidecode
 import string, time
 from pathlib import Path
+from cchardet import UniversalDetector
 
-html_list_csv = open('data/html symbol list short.csv', mode='r')
-csv_reader = csv.DictReader(html_list_csv)
+__time__ = True
+__count__ = True
 
 time_html = 0
 time_regex = 0
+count_html = 0
+count_regex = 0
 
-print_count = 0
 
-# replace html codes with respective ascii symbols
-def csv_edit(line):
-    global csv_reader
-    # TODO: find a more efficient method that recognizes patterns and records how many escape codes were replaced
+# Replace html codes and unicode symbols with respective ascii symbols
+def html_edit(line):
+    global count_html
 
     # To match letter codes:
     r"&[a-zA-Z0-9]+;"
@@ -30,73 +30,97 @@ def csv_edit(line):
     r"&#[0-9]+;"
 
     # To match all codes:
-    r"&#?[a-zA-Z0-9]+;"
+    r"&#?[a-zA-Z0-9]+?;"
 
-    row_count = 0;
-    char_i = 0;
+    # To match non-ascii characters
+    r"[^\x00-\x7F]"
 
-    # print("Line: ", line)
-    for row in csv_reader:
-        if row_count != 0:
-            line = line.replace(row["Name Code"], row["Character"])
-            line = line.replace(row["Number Code"], row["Character"])
-            # if line.find(row["Name Code"]) > -1:
-            #     new_line = line.replace(row["Name Code"], row["Character"])
-            #     print(f"Replaced {row['Description']}")
-            #     line = new_line
-            # if line.find(row["Number Code"]) > -1:
-            #     new_line = line.replace(row["Number Code"], row["Character"])
-            #     print(f"Replaced {row['Description']}")
-            #     line = new_line
-        row_count += 1
-    html_list_csv.seek(0)
+    # Remove leading spaces
+    line = line.lstrip(' \t')
+
+    # Replace html escape codes with respective symbols
+    if __count__:
+        count_html += len(re.findall(r"&#?[a-zA-Z0-9]+?;", line))
+    line = html.unescape(line)
+
+    # Replace non-ascii characters with ascii equivalent strings
+    if __count__:
+        count_html += len(re.findall(r"[^\x00-\x7F]", line))
+    line = unidecode.unidecode(line)
 
     return line
 
 
 # delete/replace markup syntax using regex
 def regex_edit(line):
-    global print_count
+    global count_regex
 
-    if print_count < 10:
-        print(repr(line))
-        print_count += 1
-
-    # List of content expressions to delete matches for - in order of precedence
-    list_delete_content = [r"<p><span class=\"pagenum\">.*?</span></p>",
-                           r"<ins class=\"(mycorr|authcorr)\".*?>.*?</ins>",
-                           r"<p class=\"illustration( chapter)?\">.*?</p>",
-                           r"<p class=\"ph3\">.*?</p>"
+    # List of complex expressions to delete matches for - in order of precedence
+    list_delete_content = [r"<p><span class=(\"|\')pagenum\1>.*?</span></p>",
+                           r"<span class=(\"|\')pagenum\1>.*?</span>",
+                           r"<p class=(\"|\')(ph2|center pfirst)\1.*?>.*?</p>",
+                           r"<(h[0-9]+?).*?>.*?</\1>",
+                           r"<ins class=(\"|\')(mycorr|authcorr)\1.*?>.*?</ins>",
+                           r"<p class=(\"|\')illustration( chapter)?\1>.*?</p>",
+                           r"<p class=(\"|\')(ph3|center pfirst)\1>.*?</p>",
+                           r"(?<=>)CHAPTER.*?(?=<)",
+                           r"<p class=\"title\">.*?\."
                            ]
 
-    # List of tag expressions to delete matches for - in order of precedence
-    list_delete_tags = [r"</?[ahpib].*?>",
-                        r"</?(small|strong|span|div|br|em|blockquote).*?>"
-                        ]
+    # List of complex expressions to delete surroundings for - in order of precedence
+    # list_delete_surround = [r"<span class=\"smcap\">(.*?)</span>"
+    #                        ]
 
-    # Remove complex tags with inner content
-    for exp in list_delete_content:
-        line = re.sub(exp, "", line)
+    # List of tag expressions to delete matches for - in order of precedence
+    list_delete_tags = [r"</?[ahpibu].*?>",
+                        r"</?(small|strong|span|div|br|em|ins|cite|blockquote).*?>"
+                        ]
 
     # (?<=x) lookbehind for x
     # (?<!x) negative lookbehind for x
     # Replace mid-sentence line breaks with spaces
+    if __count__:
+        count_regex += len(re.findall(r"(?<=.)(?<!>)[\r\n]", line))
     line = re.sub(r"(?<=.)(?<!>)[\r\n]", " ", line)
 
     # Remove all other line breaks
+    if __count__:
+        count_regex += len(re.findall(r"[\r\n]", line))
     line = re.sub(r"[\r\n]", "", line)
 
-    # Add line breaks between paragraph/header blocks
-    line = re.sub(r"</(p|h.)>", r"</\1>\r\n", line)
+    # Remove complex tags with inner content
+    for exp in list_delete_content:
+        if __count__:
+            count_regex += len(re.findall(exp, line))
+        line = re.sub(exp, "", line)
+
+    # Remove complex surrounding tags
+    # for exp in list_delete_surround:
+    #     if __count__:
+    #         count_regex += len(re.findall(exp, line))
+    #     line = re.sub(exp, r"\1", line)
+
+    # Class for alphanumeric or punctuation symbol
+    c1 = "[A-Za-z0-9_.,:;!? \"\'$]"
+    c2 = "[A-Za-z0-9_.,:;!?\"\'$]"
+
+    # Add line breaks between content blocks
+    if __count__:
+        count_regex += len(re.findall(rf"(?<={c2})</(p|u|h.|div|span)>(?!{c1}{c2})", line))
+    line = re.sub(rf"(?<={c2})</(p|u|h.|div|span)>(?!{c1}{c2})", r"</\1>\r\n", line)
+    # Explanation for this pattern:
+    # This pattern matches terminating tags surrounded by alphanumeric/punctual characters.
+    # Matches must be preceded by at least one character, and NOT followed by two.
+    # The first character after the match must, also, not be a space.
 
     # Remove all remaining tags
     for exp in list_delete_tags:
+        if __count__:
+            count_regex += len(re.findall(exp, line))
         line = re.sub(exp, "", line)
 
-    #
-    line = re.sub("æ", "ae", line)
-
     return line
+
 
 # Open a user-specified file
 while True:
@@ -104,36 +128,81 @@ while True:
     if usr == "q":
         break
     try:
-        text_read = open(usr, 'r', encoding='utf-8', errors='replace')
+        # Detect file encoding
+        file = open(usr, 'rb')
+        det = UniversalDetector()
+        for line in file.readlines():
+            det.feed(line)
+            if det.done:
+                break
+        det.close()
+        usr_enc = det.result.get("encoding")
+
+        print(f"File encoding: {usr_enc}")
+
+        text_read = open(usr, 'r', encoding=usr_enc, errors='ignore')
         text = text_read.readlines()
         text_read.close()
 
         p = Path(usr)
-        print(f"path = \"{p.parent}\", filename = \"{p.stem}\"")
+        # print(f"path = \"{p.parent}\", filename = \"{p.stem}\"")
 
         text_write = open(f"{p.parent}/{p.stem}.txt", "w+", encoding='utf-8', errors='replace')
+
+        time_html = 0
+        count_html = 0
+        time_regex = 0
+        count_regex = 0
+        time_init = 0
 
         line_count = 0
         for line in text:
             line_count += 1
 
-            # time_init = time.perf_counter()
-            # line = csv_edit(line)
-            # time_html += time.perf_counter()-time_init
+            # HTML edit section
+            if __time__:
+                time_init = time.perf_counter()
+            line = html_edit(line)
+            if __time__:
+                time_regex += time.perf_counter() - time_init
 
-            time_init = time.perf_counter()
+            # Regex edit section
+            if __time__:
+                time_init = time.perf_counter()
             line = regex_edit(line)
-            time_regex += time.perf_counter() - time_init
+            if __time__:
+                time_regex += time.perf_counter() - time_init
 
             text_write.write(line)
         text_write.close()
         print(f"Processed {line_count} lines.")
-        print(f"Replacing html escape codes took {time_html:.2f} seconds.")
-        print(f"Regex editing took {time_regex:.2f} seconds.")
+        # Debug printout section
+        if __time__ or __count__:
+
+            # HTML info section
+            print("HTML:  ", end="")
+            if __count__:
+                print(f"{count_html:-8d} codes   ", end="")
+            if __count__ and __time__:
+                print("--", end="")
+            if __time__:
+                print(f"{time_html:-6.2f} seconds ", end="")
+            print()
+
+            # Regex info section
+            print("Regex: ", end="")
+            if __count__:
+                print(f"{count_regex:-8d} matches ", end="")
+            if __count__ and __time__:
+                print("--", end="")
+            if __time__:
+                print(f"{time_regex:-6.2f} seconds ", end="")
+            print()
+
         print(f"Created file: {p.parent}/{p.stem}.txt")
 
     except FileNotFoundError:
         print("Invalid file")
     print()
 
-html_list_csv.close()
+# ...
